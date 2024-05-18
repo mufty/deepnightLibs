8cbc412271669bc98fa981c840191f63319cfd53
Michal  Gubriansky <gubriansky@sernato.sk>
2024-05-18 08:32:49 +0200

    saving and loading local achievements

--- src/dn/achievements/LocalAchivementPlatform.hx
+++ src/dn/achievements/LocalAchivementPlatform.hx
@@ -1,5 +1,6 @@
 package dn.achievements;
 
+import hxd.Save;
 import haxe.ds.StringMap;
 import dn.data.AchievementDb;
 
@@ -8,6 +9,7 @@ import dn.data.AchievementDb;
  * Save in Array
  */
 class LocalAchivementPlatform extends AbstractAchievementPlatform {
+    final LOCAL_ACHIEVEMENT_STORAGE = "localAchievements";
 	var dones : Array<String>;
 
 	public function new() {
@@ -19,7 +21,16 @@ class LocalAchivementPlatform extends AbstractAchievementPlatform {
 		if(originalStatus == null) originalStatus = [];
 		dones = originalStatus;
 	}
-	public function init() {}
+	public function init() {
+        var def = null;
+        var saved = Save.load(def, LOCAL_ACHIEVEMENT_STORAGE);
+        if(saved != null) {
+            dones = saved;
+        } else {
+            dones = [];
+
+        }
+    }
 
 	function internalClear(ach:Achievements) {
         var ok = dones.remove(ach.Id.toString());
@@ -31,7 +42,10 @@ class LocalAchivementPlatform extends AbstractAchievementPlatform {
     }
 
 	public function unlock(ach:Achievements):Bool {
-		if(!dones.contains(ach.Id.toString()))dones.push(ach.Id.toString());
+		if(!dones.contains(ach.Id.toString())) {
+            dones.push(ach.Id.toString());
+            Save.save(this.dones, LOCAL_ACHIEVEMENT_STORAGE);
+        }
 		return true;
 	}
 }
